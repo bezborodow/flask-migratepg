4594a532cc77e4a3c890ad6fba6a72b7de9763d6
Damien Bezborodov <dbezborodov@gmail.com>
2023-08-17 19:28:32 +0930

    Schema.

--- src/flask_migratepg/__init__.py
+++ src/flask_migratepg/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint, current_app
 import psycopg
 import importlib.util
 import os
+import json
 
 
 def migrate_sql(conn, e):
@@ -65,29 +66,72 @@ class MigratePg:
         if app is not None:
             self.init(app)
 
+    def connect(self):
+        return psycopg.connect(
+                current_app.config.get('PSYCOPG_CONNINFO'))
+
     def init(self, app):
         bp = Blueprint('migrate', __name__)
 
         @bp.cli.command('execute')
         def execute():
-            conninfo = current_app.config.get('PSYCOPG_CONNINFO')
-            default_migrations_path = os.path.join(current_app.root_path, 'database/migrations')
-            migrations_path = current_app.config.get('MIGRATIONS_PATH', default_migrations_path)
-            with psycopg.connect(conninfo) as conn:
+            migrations_path = current_app.config.get(
+                    'MIGRATIONS_PATH',
+                    os.path.join(current_app.root_path, 'database/migrations'))
+
+            with self.connect() as conn:
                 init(conn)
 
+                # Check for new migrations files.
                 with os.scandir(migrations_path) as d:
                     ls = list(d)
                     ls.sort(key = lambda e: e.name)
                     for e in ls:
-                        if not e.is_file():
-                            continue
-                        if e.name.startswith('.'):
-                            continue
+                        if not e.is_file() or e.name.startswith('.'):
+                            continue # Ignored file.
+
+                        # SQL migration.
                         if e.name.endswith('.sql'):
                             migrate_sql(conn, e)
+
+                        # Python migration.
                         if e.name.endswith('.py'):
                             migrate_py(conn, e)
+
+            print('Done.')
+
+        @bp.cli.command('schema')
+        def schema():
+            schema_path = current_app.config.get(
+                    'MIGRATEPG_SCHEMA_PATH',
+                    os.path.join(current_app.root_path, 'database/schema'))
+
+            if not os.path.exists(schema_path):
+                os.mkdir(schema_path)
+
+            with self.connect() as conn:
+                cur = conn.cursor()
+                cur.row_factory = psycopg.rows.namedtuple_row
+                query = '''
+                    select t.table_name as table_name,
+                        array_agg( c ) as columns
+                    from information_schema.tables t
+                    inner join (
+                        select cl.table_name, cl.column_name, cl.udt_name
+                        from information_schema.columns cl
+                    ) c (table_name,column_name,udt_name) on c.table_name = t.table_name
+                    where t.table_type = 'BASE TABLE'
+                    and t.table_schema NOT IN ('pg_catalog', 'information_schema')
+                    and t.table_catalog = current_database()
+                    group by t.table_name
+                '''
+
+                cur.execute(query)
+                while row := cur.fetchone():
+                    schema_file = os.path.join(schema_path, row.table_name + '.json')
+                    with open(schema_file, 'w') as f:
+                         json.dump(row.columns, f,
+                                   indent=2)
 
             print('Done.')
 
